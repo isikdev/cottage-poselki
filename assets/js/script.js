c8b4f91f1fed11efdce4a8d4b4c9379746eb3f45
Iskandar <isikjandev@gmail.com>
2024-08-07 02:19:15 +0500

    101%

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,22 @@ $(document).ready(function () {
     var $bankInner = $('.feedback__bank-inner');
     var $bankWrapper = $('.feedback__bank-wrapper');
 
-    // Clone the content of .feedback__bank-inner to simulate infinite scroll
     $bankInner.each(function () {
         $(this).append($(this).html());
     });
 
-    // Adjust the animation duration based on the container width
-    var originalWidth = $bankInner.width() / 2; // Since we duplicated the content
-    var duration = (originalWidth / 30) + 's'; // Adjust speed as needed
-    $bankInner.css('animation-duration', duration);
+    var originalWidth = $bankInner.width() / 2;
+
+    function animateBankInner() {
+        $bankInner.animate({ marginLeft: -originalWidth }, {
+            duration: 10000,
+            easing: 'linear',
+            complete: function () {
+                $bankInner.css('marginLeft', '0');
+                animateBankInner();
+            }
+        });
+    }
+
+    animateBankInner();
 });
